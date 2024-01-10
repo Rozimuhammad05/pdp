bd4cd19869e3c54bc480c98e4750e924b073a28d
Rozimuhammad05 <roziabdusamadov2005.10@gmail.com>
2024-01-10 17:05:28 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,138 @@
+// var text = "hello world";
+
+// console.log(text.charAt(5));
+
+// console.log(text[0]);
+
+// console.log(text.charCodeAt(0));
+
+// console.log(text.indexOf("t", 25));
+
+// console.log(text.concat(" bu matnga oid ma'lumot turi"));
+
+// console.log(text);
+
+// console.log(text.startsWith("He", 0));
+
+// console.log(text.endsWith("a", 17));
+
+// console.log(text.length);
+
+
+// console.log(text.trim());
+
+// ES10 2019
+// console.log(text.trimStart());
+// console.log(text.trimEnd());
+
+// console.log(text.replace(" ", ""));
+
+// console.log(text.replaceAll("", ""));
+
+// 0123456789
+// flow
+
+// var text = "hello, world, dsAdsa asdAsd 25 adsdAsd adsdAsas ";
+
+// console.log(text.lastIndexOf(" ", 6)); -1
+
+// console.log(text.includes("l", 6)); boolean
+
+// console.log(text.match(/j/gi));
+
+// console.log(text.split(/a/gi)); split splitter seperator(regex|searchString, limit) => [array]
+
+// console.log(text);
+
+// var  number = 5545454515;
+
+// console.log(Number(number));
+
+// console.log(parseInt(number));
+
+// var firstValue = Number(prompt("1-raqamni kiriting"));
+// var secondValue = Number(prompt("2-raqamni kiriting"));
+// var thirdValue = Number(prompt("3-raqamni kiriting"));
+
+// var result = Math.max(firstValue, secondValue, thirdValue);
+
+// console.log(firstValue + " " + secondValue + " " + thirdValue + "\t" + result);
+
+
+// Boolean = true / false
+
+// var userAge = 18;
+
+// condition == shart
+
+// >
+// <
+// >=
+// <=
+// ==
+// ===
+// !=
+// value == value
+// value and type === value and type
+
+
+// Logic operators
+// &&
+// ||
+// !
+
+
+// true
+
+// if("20" > "20") {
+//   console.log("Voyaga yetgan shaxs");
+// } else {
+//   console.log("Yosh bola");
+// }
+
+// var randomNumber = Number(prompt("Enter number"));
+
+// && and  return first false + lastest true
+// || or return first true + lastest false
+
+// ! not equal
+// condation chain
+
+// debugger;
+// performance
+// ninja code
+
+// switch case
+// ? : :  : :
+
+// if(randomNumber % 3 == 0 || randomNumber % 5 == 0) {
+//   console.log("FizzBuzz");
+// } else if(randomNumber % 3 == 0) {
+//   console.log("Fizz");
+// } else if(randomNumber % 5 == 0) {
+//   console.log("Buzz");
+// } else {
+//   console.log(randomNumber + " na 3 va na 5 bo'linmaydi !");
+// }
+
+// Truth and Falsy value
+
+// console.log(Boolean(false));
+
+// console.log(Boolean(0));
+// console.log(Boolean(""));
+// console.log(Boolean(undefined));
+// console.log(Boolean(null));
+// console.log(Boolean(NaN));
+
+
+
+// var a;
+
+var elText = document.querySelector(".text");
+
+// console.log(elText.textContent.trim());
+
+elText.textContent = "JS";
+
+document.write("dsadasdsadsadsdsa");
